c2b6037d23ff32e72241421e23da896485793d75
jskimweb <jskimweb@gmail.com>
2024-08-30 12:25:56 +0900

    Add component 'BookItem' & style home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,26 @@
 import SearchableLayout from "@/components/SearchableLayout";
 import { type ReactNode } from "react";
+import style from "@/styles/index.module.css";
+import books from "@/mock/books.json";
+import BookItem from "@/components/BookItem";
 
 export default function Home() {
-  return <>Home</>;
+  return (
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+    </div>
+  );
 }
 
 Home.getLayout = (page: ReactNode) => {
