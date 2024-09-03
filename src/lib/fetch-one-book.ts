e9f7c8dc8ab1b0c7e6718c1d821a231c5dd83dae
jskimweb <jskimweb@gmail.com>
2024-09-03 22:38:55 +0900

    Update base url

--- src/lib/fetch-one-book.ts
+++ src/lib/fetch-one-book.ts
@@ -3,7 +3,7 @@ import type { BookData } from "@/types";
 export default async function fetchOneBook(
   id: number
 ): Promise<BookData | null> {
-  const url = `http://localhost:12345/book/${id}`;
+  const url = `https://onebite-books-server-three.vercel.app/book/${id}`;
 
   try {
     const res = await fetch(url);
